80e051e3fbb38ed36b00f12809fafa254cedd450
sawany3821 <ghepardo0418@gmail.com>
2023-08-15 15:29:24 +0900

    complete chapter4

--- finchart/views.py
+++ finchart/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import generic
+from django.views.generic import TemplateView
+
+class IndexTemplateView(TemplateView):
+    template_name = 'finchart/index.html'
