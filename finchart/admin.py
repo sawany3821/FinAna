7442f0b10c067ae9db49b969484dcd47c67f73ff
sawany3821 <ghepardo0418@gmail.com>
2023-08-16 18:46:15 +0900

    complete chapter5

--- finchart/admin.py
+++ finchart/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Company, Fstatement
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_display_links = ('name',)
+
+
+@admin.register(Fstatement)
+class FstatementAdmin(admin.ModelAdmin):
+    list_display = ('company', 'fiscal_year')
+    list_display_links = ('company',)
+
--- finchart/views.py
+++ finchart/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
 from django.views import generic
 from django.views.generic import TemplateView
+from .models import Fstatement
 
-class IndexTemplateView(TemplateView):
+class IndexView(TemplateView):
     template_name = 'finchart/index.html'
+
+    def get_context_data(self, **kwargs):
+        fstatement_list = Fstatement.objects.all().order_by('company')
+        params = {
+            'fstatement_list': fstatement_list,
+        }
+        return params
